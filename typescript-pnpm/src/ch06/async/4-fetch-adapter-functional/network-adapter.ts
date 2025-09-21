61e9ff41de9d53353da91eb692e1dcc34854fb0e
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-09-21 13:03:59 +0900

    funtional inject test

---
+++ typescript-pnpm/src/ch06/async/4-fetch-adapter-functional/network-adapter.ts
@@ -0,0 +1,12 @@
+import fetch, { Response } from "node-fetch";
+
+export const fetchUrlText = async(url: string) => {
+  const resp: Response = await fetch(url);
+
+  if (resp.ok) {
+    const text = await resp.text();
+    return { ok: true, text };
+  }
+
+  return { ok: false, text: resp.statusText };
+};
---
+++ typescript-pnpm/src/ch06/async/4-fetch-adapter-functional/website-verifier.ts
@@ -0,0 +1,22 @@
+export const isWebsiteAlive = async(network: { fetchUrlText: (arg: string) => { ok: boolean, text: string }; }) => {
+  const result = await network.fetchUrlText("http://example.com");
+
+  if (result.ok) {
+    const text = result.text;
+    return onFetchSuccess(text);
+  }
+
+  return onFetchError(result.text);
+};
+
+const onFetchSuccess = (text: string) => {
+  const included = text.includes("illustrative");
+  if (included) {
+    return { success: true, status: "ok" };
+  }
+  return { success: false, status: "missing text" };
+};
+
+const onFetchError = (err: unknown) => {
+  return { success: false, status: err };
+};
