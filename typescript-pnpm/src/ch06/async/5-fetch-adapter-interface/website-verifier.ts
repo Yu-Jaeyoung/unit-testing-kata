da245c2747882a4c858f0ba6e8bff66d61b37caa
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-09-21 13:40:42 +0900

    object, interface based adapter

---
+++ typescript-pnpm/src/ch06/async/5-fetch-adapter-interface/website-verifier.ts
@@ -0,0 +1,40 @@
+import { INetworkAdapter, NetworkAdapterFetchResults } from "./network-adapter";
+
+export interface WebsiteAliveResult {
+  success: boolean;
+  status: string;
+}
+
+export class WebsiteVerifier {
+  constructor(private network: INetworkAdapter) {}
+
+  isWebsiteAlive = async(): Promise<WebsiteAliveResult> => {
+    let netResult: NetworkAdapterFetchResults;
+
+    try {
+      netResult = await this.network.fetchUrlText("http://example.com");
+      if (!netResult.ok) {
+        throw netResult.text;
+      }
+
+      const text = netResult.text;
+      return this.processNetSuccess(text);
+    } catch (err) {
+      throw this.processNetFail(err as string);
+    }
+  };
+
+  processNetSuccess = (text: string): WebsiteAliveResult => {
+    const included = text.includes("illustrative");
+
+    if (included) {
+      return { success: true, status: "ok" };
+    }
+
+    return { success: false, status: "missing text" };
+  };
+
+  processNetFail = (err: string): WebsiteAliveResult => {
+    return { success: false, status: err };
+  };
+}
